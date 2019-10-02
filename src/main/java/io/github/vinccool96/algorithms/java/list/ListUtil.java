1a6fbe4da1fcfa0e0e0a3edf541e544ea469899b
Vincent Girard <vincent.girard.7@ulaval.ca>
2019-10-02 09:23:06 -0400

    Added documentation

--- src/main/java/io/github/vinccool96/algorithms/java/list/ListUtil.java
+++ src/main/java/io/github/vinccool96/algorithms/java/list/ListUtil.java
@@ -25,7 +25,7 @@ public class ListUtil {
      * @param list the {@link List<T>} that will get its elements swapped
      * @param index1 the index of the first object to swap
      * @param index2 the index of the second object to swap
-     * @param <T> the type of the object in {@code list}
+     * @param <T> the type of the objects in {@code list}
      */
     public <T> void swap(List<T> list, int index1, int index2) {
         T temp = list.set(index1, list.get(index2));
@@ -38,12 +38,14 @@ public class ListUtil {
      * {@link List#add(Object)} will be performed to add {@code null}, then {@code list.add(element)} so that
      * {@code element} will be at the index {@code index} in {@code list}.
      *
-     * @param list
-     * @param index
-     * @param element
-     * @param <T>
+     * @param list the {@link List<T>} to which the element will be set or added to the specified position.
+     * @param index the index of the element
+     * @param element the element to put in the list
+     * @param <T> the type of {@code element}
      *
-     * @return
+     * @return if {@code list} was long enough to have something at the index {@code index}, the element that was
+     * previously there will be returned, just like with {@link List#set(int, Object)}. If it wasn't, {@code null} will
+     * be returned.
      */
     public <T> T setOrAdd(List<T> list, int index, T element) {
         T returned = null;
@@ -63,7 +65,7 @@ public class ListUtil {
      *
      * @param listFrom the {@link List<T>} to copy to {@code listTo}
      * @param listTo the {@link List<T>} in which the elements of {@code listFrom} are put.
-     * @param <T> the type of the object in {@code list}
+     * @param <T> the type of the objects in {@code list}
      */
     public <T> void copyList(List<T> listFrom, List<T> listTo) {
         listTo.clear();
@@ -75,10 +77,10 @@ public class ListUtil {
      *
      * @param listFrom the {@link List<T>} to copy to {@code listTo}
      * @param listTo the {@link List<T>} in which the elements of {@code listFrom} are put.
-     * @param <T> the type of the object in {@code list}
+     * @param <T> the type of the objects in {@code list}
      */
     public <T> void copyList(List<T> listFrom, List<T> listTo, int indexEnd) {
-        if (indexEnd>=listFrom.size()){
+        if (indexEnd >= listFrom.size()) {
             String s = "indexEnd can't be greater or equal to listFrom.size()";
             throw new IllegalArgumentException(s);
         }
@@ -92,10 +94,11 @@ public class ListUtil {
      * {@code indexEnd}. {@code indexEnd} is included if {@code endIncluded} is {@code true}.
      *
      * @param listFrom the {@link List<T>} to copy to {@code listTo}
-     * @param listTo
-     * @param indexStart
-     * @param indexEnd
-     * @param <T>
+     * @param listTo the {@link List<T>} in which the elements of {@code listFrom} are put.
+     * @param indexStart the starting index of the elements to put in {@code listTo}
+     * @param indexEnd the last index of the elements to put in {@code listTo}
+     * @param endIncluded if {@code true}, {@code indexEnd} is included
+     * @param <T> the type of the objects in {@code list}
      */
     public <T> void copyList(List<T> listFrom, List<T> listTo, int indexStart, int indexEnd, boolean endIncluded) {
         if (indexStart < 0 || indexStart >= listFrom.size() || indexEnd < 0 || indexEnd >= listFrom.size()) {
@@ -112,9 +115,11 @@ public class ListUtil {
             }
             throw new IndexOutOfBoundsException(s);
         }
-        if ((endIncluded && indexStart > indexEnd) || (!endIncluded && indexStart >= indexEnd)) {
-            String s = endIncluded ? "indexStart can't be higher than indexEnd" :
-                    "indexStart can't be higher or equal to than indexEnd";
+        if ((endIncluded && indexStart > indexEnd) || (!endIncluded && indexStart >= indexEnd) ||
+                listFrom.size() != listTo.size()) {
+            String s = listFrom.size() != listTo.size() ? "listFrom and listTo must be the same size" :
+                    endIncluded ? "indexStart can't be higher than indexEnd" :
+                            "indexStart can't be higher or equal to than indexEnd";
             throw new IllegalArgumentException(s);
         }
         for (int i = indexStart; i < (endIncluded ? indexEnd + 1 : indexEnd); i++) {
